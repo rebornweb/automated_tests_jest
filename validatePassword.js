b20bfea25de3f4f1c348aad45f8401ce116b7cb8
Andrei <andrei.nicolas@hotmail.com>
2023-04-03 19:17:13 +1200

    Failed password length test

--- validatePassword.js
+++ validatePassword.js
@@ -1,5 +1,6 @@
 function validatePassword(password) {
   const validLength = password.length >= 8
+  console.log(validLength);
   let hasLetter = /[a-zA-Z]/g.test(password)
   let hasNumber = /[0-9]/g.test(password)
   return hasNumber && hasLetter && validLength
